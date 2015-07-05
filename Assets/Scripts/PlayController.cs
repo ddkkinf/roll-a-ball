95e1dcd3c1f32fbe80e38d3aba7b0f4475a78847
ddkkinf <ddkkinf@naver.com>
2015-07-06 00:30:23 +0900

    Roll a Ball 예제 완료

--- Assets/Scripts/PlayController.cs
+++ Assets/Scripts/PlayController.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayController : MonoBehaviour {
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
+	private int count;
 
 	void Start() {
 		rb = GetComponent<Rigidbody>();
+		setCountText(0);
+		winText.text = "";
 	}
 
 	void FixedUpdate() {
@@ -22,6 +28,15 @@ public class PlayController : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag("Pick Up")) {
 			other.gameObject.SetActive(false);
+			setCountText(count + 1);
+		}
+	}
+
+	void setCountText(int count) {
+		this.count = count;
+		countText.text = "Count : " + count.ToString();
+		if (count >= 10) {
+			winText.text = "You Win!";
 		}
 	}
 }
